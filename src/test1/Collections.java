df156cc4aef486b5f875c4c6996fa2b273e149ad
khaoulani.sohayb@gmail.com <admin@dfef-PC>
2016-09-15 17:02:16 +0200

    Première version (SohaybListe)

---
+++ src/test1/Collections.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+public class Collections {
+    public static void main(String[] args) {
+//        ArrayList<String> list = new ArrayList<String>();
+        ArrayList<Integer> i= new ArrayList<Integer> ();
+        
+        for( int i = 2; i <=100; i =i+2 ){
+            
+            
+        }
+        
+        i.add(Integer.)
+//        list.add("A");
+//        list.add("B");
+//        list.add("B");
+//        list.add("C");
+////        for (String s: list){
+//            System.out.println(list );
+//            list.remove("Coucou");
+////            System.out.println(list + "" +list.size());
+//list.removeAll(list);
+//            System.out.println(list.get(2));
+         
+        }
+    }
+
