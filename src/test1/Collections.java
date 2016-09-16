86d62eed514f7f11c514031617aafadce4666725
khaoulani.sohayb@gmail.com <admin@dfef-PC>
2016-09-16 09:23:54 +0200

    Première version (SohaybListe)

--- src/test1/Collections.java
+++ src/test1/Collections.java
@@ -13,15 +13,43 @@ import java.util.ArrayList;
  */
 public class Collections {
     public static void main(String[] args) {
-//        ArrayList<String> list = new ArrayList<String>();
-        ArrayList<Integer> i= new ArrayList<Integer> ();
-        
-        for( int i = 2; i <=100; i =i+2 ){
-            
-            
+//     public class Collections {
+
+    
+        ArrayList<Integer> list1 = new ArrayList<>();
+
+        for (int i = 10; i <= 100; i = i + 10) {
+            list1.add(i);
         }
-        
-        i.add(Integer.)
+
+//            System.out.println(i+"|");
+            ArrayList<Integer> list2 = new ArrayList<>();
+
+            for (int j = 2; j <= 100; j = j + 2) {
+                list2.add(j);
+            }
+           
+                ArrayList<Integer> list3 = new ArrayList<>();
+
+                for (int z = 3; z <= 99; z = z + 3) {
+                    list3.add(z);
+                }
+//                    System.out.println( i);
+                
+      
+    list2.removeAll(list1);
+    list2.removeAll(list3);
+System.out.println(list1);
+                 System.out.println( list2);
+                System.out.println(list3);
+        }
+    }
+
+
+//list1.add(i);
+//           list2.add(j);
+//           list3.add(z);
+//        i.add(Integer.)
 //        list.add("A");
 //        list.add("B");
 //        list.add("B");
@@ -32,7 +60,4 @@ public class Collections {
 ////            System.out.println(list + "" +list.size());
 //list.removeAll(list);
 //            System.out.println(list.get(2));
-         
-        }
-    }
 
